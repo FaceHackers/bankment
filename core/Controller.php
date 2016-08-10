c4e5aa4d04463cbaf3f05f7762f52217114510c7
David_Lai <cool72456@gmail.com>
2016-08-10 02:52:29 +0000

    使用MVC架構核心及修改檔名

---
+++ core/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+class Controller
+{
+    public function model($model)
+    {
+        require_once "core/MyPDO.php";
+        require_once "models/$model.php";
+        return new $model ();
+    }
+    public function view($view, $data = Array())
+    {
+        $config = $this->config();
+        require_once "views/$view.php";
+    }
+    public function config(){
+        return new config();
+    }
+}
